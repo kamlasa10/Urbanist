5e9a2de46e208383368f5f263100efd0983f5b42
Vlad Verstalshik <kamlasa@ukr.net>
2021-06-25 16:20:14 +0300

    fixed home-page

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -1,1 +1,14 @@
 @@include('../libs/libs.js')
+
+function showMenu() {
+    $('.js-btn-menu').on('click', e => {
+        e.preventDefault()
+        
+        $('.js-btn-menu').toggleClass('active')
+        $('.js-menu').toggleClass('show')
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showMenu()
+})
