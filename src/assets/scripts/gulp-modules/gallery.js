5212abfb91be3fcc31f4ce8509997415b9bee754
Vlad <kamlasa@ukr.net>
2021-06-29 22:45:58 +0300

    fixed slider

--- src/assets/scripts/gulp-modules/gallery.js
+++ src/assets/scripts/gulp-modules/gallery.js
@@ -1,86 +1,95 @@
 class InitDoubleSliders {
-    constructor({$slider1, $slider2}) {
-        this.$slider1 = $slider1
-        this.$slider2 = $slider2
-    }
+  constructor({ $slider1, $slider2 }) {
+    this.$slider1 = $slider1;
+    this.$slider2 = $slider2;
+  }
 
-    init() {
-        this.$slider2 = new Swiper(this.$slider2, {
-            direction: 'vertical',
-            loop: true,
-            slidesPerView: 5,
-            centeredSlides: true,
-            on: {
-                slideChangeTransitionStart(e) {
-                    
-                },
-                init(swiper) {
-                    swiper.slides.forEach(item => {
-                        item.addEventListener('click', e => {
-                            const clickedEl = e.currentTarget
-                            const activeSlide = swiper.$el.find('.swiper-slide-active')
+  init() {
+    this.$slider2 = new Swiper(this.$slider2, {
+      direction: 'vertical',
+      loop: true,
+      slidesPerView: 5,
+      centeredSlides: true,
+      on: {
+        init(swiper) {
+          swiper.slides.forEach(item => {
+            item.addEventListener('click', e => {
+              const activeSlide = $(e.currentTarget)
+              const neighborBottom = activeSlide.next()
+              const neighborTop = activeSlide.prev()
+              if(neighborTop.hasClass('swiper-slide-active')) {
+                gsap.to(neighborTop, {
+                  top: neighborTop[0].getBoundingClientRect().height + 'px',
+                  duration: 0.7,
+                });
+                gsap.to(activeSlide, {
+                  top: '-' + activeSlide[0].getBoundingClientRect().height + 'px',
+                  duration: 0.7,
+                });
+              } else {
+                gsap.to(neighborBottom, {
+                  top: -neighborBottom[0].getBoundingClientRect().height + 'px',
+                  duration: 0.7,
+                });
+                gsap.to(activeSlide, {
+                  top: activeSlide[0].getBoundingClientRect().height + 'px',
+                  duration: 0.7,
+                });
+              }
+            });
+          });
+        },
+      },
+    });
 
-                            swiper.slides.forEach(item => item.classList.remove('swiper-slide-active'))
-                            clickedEl.classList.add('swiper-slide-active')
-                            
 
-                            gsap.to(clickedEl, {
-                                top: (activeSlide[0].getBoundingClientRect().bottom - clickedEl.getBoundingClientRect().bottom) + 'px',
-                                duration: 0.8
-                            })
-                            console.log(clickedEl.getBoundingClientRect(), activeSlide[0].getBoundingClientRect())
-                            gsap.to(activeSlide, {
-                                top: (clickedEl.getBoundingClientRect().bottom - activeSlide[0].getBoundingClientRect().bottom) + 'px',
-                                duration: 0.8
-                            })
-                        })
-                    })
-                }
-            }
-        })
 
-        this.$slider1 = new Swiper(this.$slider1, {
-            loop: true,
-            speed: 700,
-            navigation: {
-                nextEl: '.js-swiper-button-next_gallery',
-                prevEl: '.js-swiper-button-prev_gallery'
-            },
-            thumbs: {
-                swiper: this.$slider2
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'fraction',
-                formatFractionCurrent: function (number) {
-                    let isWithZero = number < 10 ? `0${number}`: number + ''
-                    return isWithZero.slice(-2);
-                },
-                formatFractionTotal: function (number) {
-                    let isWithZero = number < 10 ? `0${number}`: number + ''
-                    return isWithZero.slice(-2);
-                },
-                renderFraction: function (currentClass, totalClass) {
-                    return '<span class="' + currentClass + '"></span>' +
-                            ' - ' +
-                            '<span class="' + totalClass + '"></span>';
-                }
-            }
-        })
-    }
+    this.$slider2.on('slideChange', swiper => {
+      console.log(swiper)
+    })
+    this.$slider1 = new Swiper(this.$slider1, {
+      loop: true,
+      speed: 400,
+      navigation: {
+        nextEl: '.js-swiper-button-next_gallery',
+        prevEl: '.js-swiper-button-prev_gallery',
+      },
+      thumbs: {
+        swiper: this.$slider2,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+        formatFractionCurrent(number) {
+          const isWithZero = number < 10 ? `0${number}` : `${number}`;
+          return isWithZero.slice(-2);
+        },
+        formatFractionTotal(number) {
+          const isWithZero = number < 10 ? `0${number}` : `${number}`;
+          return isWithZero.slice(-2);
+        },
+        renderFraction(currentClass, totalClass) {
+          return (
+            `<span class="${currentClass}"></span>` + ' - ' + `<span class="${totalClass}"></span>`
+          );
+        },
+      },
+    });
+  }
 }
 
-const initDoubleSliders = new InitDoubleSliders(
-    {$slider1: '.js-gallery-main', $slider2: '.js-gallery-thumbs'})
+const initDoubleSliders = new InitDoubleSliders({
+  $slider1: '.js-gallery-main',
+  $slider2: '.js-gallery-thumbs',
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-    initDoubleSliders.init()
-})
-
+  initDoubleSliders.init();
+});
 
 // function initSliderGallery() {
 //   const galleryTop = new Swiper('.gallery-top', {
-//     slidesPerView: 1,  
+//     slidesPerView: 1,
 //     loop: true,
 //     loopedSlides: 50,
 //     navigation: {
@@ -104,8 +113,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // // document.addEventListener('DOMContentLoaded', () => {
 // //   var galleryTop = new Swiper('.gallery-top', {
-    
-// //     slidesPerView: 1,  
+
+// //     slidesPerView: 1,
 // //     loop: true,
 // //     navigation: {
 // //       nextEl: '.swiper-button-next_gallery',
@@ -136,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // //   });
 // //   // galleryTop.controller.control = galleryThumbs;
 // //   galleryTop.on('activeIndexChange', (el)=>{
-    
+
 // //     galleryThumbs.slideTo(galleryTop.activeIndex, 2000,false);
 // //   })
 // //   galleryThumbs.on('activeIndexChange', (el)=>{
@@ -148,10 +157,9 @@ document.addEventListener('DOMContentLoaded', () => {
 // //   // galleryThumbs.controller.control = galleryTop;
 // // })
 
-
 // document.addEventListener('DOMContentLoaded', () => {
 //   var galleryTop = new Swiper('.gallery-top', {
-//     slidesPerView: 1,  
+//     slidesPerView: 1,
 //     loop: true,
 //     navigation: {
 //       nextEl: '.swiper-button-next_gallery',
